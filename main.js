9991256ccffc214e9d4e213365ab59b0b6a1f190
Pasky2 <pascalosemene@gmail.com>
2024-02-12 19:14:36 +0100

    My first push

---
+++ main.js
@@ -0,0 +1,32 @@
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let box = document.querySelector('.box');
+let image = document.querySelectorAll('.image');
+
+console.log(image);
+
+let degrees = 0;
+
+prev.addEventListener('click', function(){
+    degrees += 45;
+    box.style = `transform: perspective(1000px) rotateY(${degrees}deg)`;
+})
+
+next.addEventListener('click', function(){
+    degrees -= 45;
+    box.style = `transform: perspective(1000px) rotateY(${degrees}deg)`;
+})
+
+
+
+for (let i = 0; i < image.length; i++) {
+    image[i].addEventListener('mouseover', function(){
+            image[i].classList.add('zoom');
+        })
+}
+
+for (let i = 0; i < image.length; i++) {
+    image[i].addEventListener('mouseout', function(){
+            image[i].classList.remove('zoom');
+        })
+}
